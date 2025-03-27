0564d0c4226d5639603483c112938cd018f450dd
Cyber Nova <saei07@naver.com>
2025-03-27 17:54:10 +0900

    aioptimizer

--- src/components/AIOptimizer/AIOptimizer.tsx
+++ src/components/AIOptimizer/AIOptimizer.tsx
@@ -1,10 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { ComponentType, ComponentOptionsTypeMap } from "../../types/index";
+import axios from "axios";
 
-interface AIOptimaizerProps {
-  currentCode: string;
-  componentType: ComponentType;
-  options: ComponentOptionsTypeMap[ComponentType];
-  codeFormat: "react" | "html";
+interface AIOptimizerProps {
+  currentCode: string; // 현재 코드
+  componentType: ComponentType; // 디자인 유형
+  options: ComponentOptionsTypeMap[ComponentType]; // 디자인 유형 상세 정의
+  codeFormat: "react" | "html"; // 디자인 코드로 나타내는 2종류
   onApplyOptimized: (optimizedCode: string) => void;
 }
+
+function AIOptimizer({
+  currentCode,
+  componentType,
+  options,
+  codeFormat,
+  onApplyOptimized,
+}: AIOptimizerProps) {
+  const [prompt, setPrompt] = useState(""); // 최적화 코드
+  const [generatedText, setGeneratedText] = useState(""); // 원본코드
+  const [loading, setLoading] = useState(false); // 로딩
+  const [error, setError] = useState("");
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    setError("");
+    setGeneratedText("");
+
+    const optimizationPrompt = `다음 ${codeFormat} 코드를 \n\n\`\`\`${currentCode}\`\`\`\n\n 을 ${componentType}디자인 유형에 맞추어서 ${JSON.stringify(
+      options
+    )} 에 맞춰 최적화로 진행`;
+    setPrompt(optimizationPrompt);
+
+    try {
+      const response = await axios.post("/api/gemini", {
+        prompt: optimizationPrompt,
+      });
+
+      setGeneratedText(response.data);
+      onApplyOptimized(response.data);
+    } catch (error: any) {
+      setError(error.response?.data?.error || "코드 생성 실패");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleSubmit} disabled={loading}>
+        Ai 최적화 버튼
+      </button>
+    </div>
+  );
+}
+
+export default AIOptimizer;
